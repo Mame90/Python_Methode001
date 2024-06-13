44f910edebf4f49e2f02b31322e0f7af08dec25c
“[mbaye98]” <mbayediop2309481@gmail.com>
2024-06-13 19:02:00 +0200

    3 eme Commit

--- Methode.py
+++ Methode.py
@@ -1,22 +1,58 @@
 #coding:utf-8
+"""
+Methode            : Fonction sur une instance (objet)
+Methode de classe  : Fonction sur une classe
+Methode statique   : Fonction  independant mais  lie a une classe
+"""
+
 
 class Humain:
   """Classe qui definit un humain"""
+  lieu_habitation="terre"
 
   def __init__(self,nom,age):
     self.nom=nom
     self.age=age
     
-  def parler(self,message):
+  def parler(self,message):    #self Methode standart
     print("{} a dit: {}".format(self.nom, message))
-
-
-
+    
+  def changer_planete(cls, nouvelle_planete):    #cls = methode de classe
+  
+    Humain.lieu_habitation=nouvelle_planete
+  changer_planete=classmethod(changer_planete)
 #Programme principal
-h1=Humain('Mbaye Diop',26)
-h1.parler('Bonjour a tous le monde ! :)')
+# h1=Humain('Mbaye Diop',26)
+# h1.parler('Bonjour a tous le monde ! :)')
+# h1.parler('Comment allez vous ! :)')
+print("planete actuelle: {}".format(Humain.lieu_habitation))
+Humain.changer_planete("Mars")
+print("Planete actuelle: {}".format(Humain.lieu_habitation))
 
+class MathUtils:
+    @staticmethod
+    def add(a, b):
+        return a + b
 
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+# Utilisation des méthodes statiques sans créer une instance de la classe
+print(MathUtils.add(5, 3))         # Affiche 8
+print(MathUtils.subtract(10, 4))   # Affiche 6
+print(MathUtils.multiply(2, 7))    # Affiche 14
+print(MathUtils.divide(15, 3))     # Affiche 5.0
 
 
 
